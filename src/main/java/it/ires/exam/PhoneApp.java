8a41040f665b8da96943b469d975831622943beb
Cristina Acsinte <cristtina.acsinte@gmail.com>
2023-07-24 12:35:23 +0200

    Seconda bozza parte uno/due

--- src/main/java/it/ires/exam/PhoneApp.java
+++ src/main/java/it/ires/exam/PhoneApp.java
@@ -2,16 +2,24 @@ package it.ires.exam;
 
 import java.time.LocalDateTime;
 import java.time.chrono.ChronoLocalDateTime;
+import java.util.Map;
 
 public class PhoneApp {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
         Sim sim = new Sim("333333", 20);
-        Telefonata telefonata = new Telefonata(0, 60, "33334455");
-        System.out.println(sim.getCredit());
-        System.out.println(telefonata.durataChiamata());
-        System.out.println(telefonata.costoChiamataAlMinuto(telefonata, sim));
+        Telefonata telefonata = new Telefonata( "33334455");
+        Telefonata telefonata2 = new Telefonata( "33334458");
+        Smarthphone smarthphone=new Smarthphone();
+        System.out.println(smarthphone.doTelefonata(telefonata));
+        //System.out.println(smarthphone.doTelefonata(telefonata2));
+        System.out.println(smarthphone.closeTelefonata(telefonata));
+        System.out.println(smarthphone.closeTelefonata(telefonata2));
+
+        System.out.println(smarthphone.durataChiamata(telefonata));
+        System.out.println(smarthphone.durataChiamata(telefonata2));
+
     }
 
 
