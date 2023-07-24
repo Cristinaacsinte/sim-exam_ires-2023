a6fd5f487f4c4c1e6954300a406f1f65d2768df8
Cristina Acsinte <cristtina.acsinte@gmail.com>
2023-07-24 11:23:14 +0200

    Prima bozza parte uno

--- src/main/java/it/ires/exam/PhoneApp.java
+++ src/main/java/it/ires/exam/PhoneApp.java
@@ -1,13 +1,18 @@
 package it.ires.exam;
 
-import it.ires.exam.dataobject.OperatorPlan;
-import it.ires.exam.dataobject.SimCard;
+import java.time.LocalDateTime;
+import java.time.chrono.ChronoLocalDateTime;
 
 public class PhoneApp {
 
-  public static void main(String[] args) {
-    OperatorPlan plan = new OperatorPlan("standard", 15, 15);
-    SimCard sim = new SimCard("+39 123 4567 890", plan, 100);
-  }
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        Sim sim = new Sim("333333", 20);
+        Telefonata telefonata = new Telefonata(0, 60, "33334455");
+        System.out.println(sim.getCredit());
+        System.out.println(telefonata.durataChiamata());
+        System.out.println(telefonata.costoChiamataAlMinuto(telefonata, sim));
+    }
+
 
 }
